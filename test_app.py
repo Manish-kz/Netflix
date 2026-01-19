b841713a1fae9bed9900031dffb1d19bd3e9071f
manish-kz <manishkhadka1970@gmai.com>
2026-01-19 12:44:08 +0530

    firsts

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 from app import app
 
-def test_home_page():
+def test_health():
     client = app.test_client()
-    response = client.get("/")
+    response = client.get("/health")
     assert response.status_code == 200
+    assert response.json["status"] == "UP"
